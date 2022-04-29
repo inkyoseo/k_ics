6eadd9059c59b0d6aa1e113d716563e016e7f254
inkyoseo <saco010@hanyang.ac.kr>
2022-04-29 22:56:20 +0900

    initial commit
    the initial file

---
+++ R/square_val.R
@@ -0,0 +1,13 @@
+#' Square a value
+#'
+#' @param x A numeric value
+#'
+#' @return a number
+#' @export
+#' @importFrom dplyr %>%
+#' @import dplyr ( >= 0.8.5)
+#' @examples
+#' square_val(2)
+square_val <- function(x) {
+  x^2
+}
---
+++ tests/testthat/test-square_val.R
@@ -0,0 +1,3 @@
+test_that("square_val actually squares", {
+  expect_equal(square_val(2), 4)
+})
